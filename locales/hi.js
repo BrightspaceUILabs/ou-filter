193ca5a6c027e84703eb7d1eb2cecd731fdda4ae
JenkinsReader <resupport@desire2learn.com>
2022-02-24 18:48:49 -0500

    Translation Updates

---
+++ locales/hi.js
@@ -0,0 +1,21 @@
+/* eslint quotes: 0 */
+
+export default {
+	"orgUnitFilter:nameAllSelected": "संगठन इकाई: सभी",
+	"orgUnitFilter:nameSomeSelected": "संगठन इकाई: चयन लागू किए गए",
+	"orgUnitFilter:name": "संगठन इकाई",
+
+	"treeFilter:nodeName": "{orgUnitName} (Id: {id})",
+	"treeFilter:nodeName:root": "रूट",
+	"treeSelector:filterBy": "इसके अनुसार फ़िल्टर करें",
+	"treeSelector:clearLabel": "साफ़ करें",
+	"treeSelector:searchLabel": "खोजें",
+	"treeSelector:loadMoreLabel": "अधिक लोड करें",
+	"treeSelector:parentLoadMore:ariaLabel": "और बाल संगठन इकाइयाँ लोड करें",
+	"treeSelector:searchLoadMore:ariaLabel": "और खोज परिणामों को लोड करें",
+	"treeSelector:searchPlaceholder": "खोजें...",
+	"treeSelector:dropdownAction": "{name} फ़िल्टर खोलें",
+	"treeSelector:arrowLabel:closed": "{name} को स्तर {level} पर बड़ा करें, {parentName} का चाइल्ड",
+	"treeSelector:arrowLabel:open": "{name} को स्तर {level} पर छोटा करें, {parentName} का चाइल्ड",
+	"treeSelector:node:ariaLabel": "{Name}, {parentName} का चाइल्ड,"
+};
