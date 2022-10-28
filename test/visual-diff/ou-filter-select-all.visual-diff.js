b395d7cef92dde8f8db922744233033180b548ad
Oscar Elhanafey <oscar.elhanafey@d2l.com>
2022-10-28 13:45:54 -0400

    Make visual diffs rerunnable

--- test/visual-diff/ou-filter-select-all.visual-diff.js
+++ test/visual-diff/ou-filter-select-all.visual-diff.js
@@ -10,22 +10,21 @@ import { VisualDiff } from '@brightspace-ui/visual-diff';
 		let browser, page;
 
 		before(async() => {
-			browser = await puppeteer.launch({ headless: true }); // { headless: false } - useful for debugging
+			browser = await puppeteer.launch({ headless: false }); // { headless: false } - useful for debugging
 			page = await visualDiff.createPage(browser);
-			await page.setViewport({
-				width: 600,
-				height: 700,
-				deviceScaleFactor: 2
-			});
+		});
+
+		after(() => browser.close());
+
+		beforeEach(async() => {
 			await page.goto(
 				`${visualDiff.getBaseUrl()}/test/visual-diff/d2l-labs-ou-filter.visual-diff.html#select-all-ui=1;dir=${dir}`,
 				{ waitUntil: ['networkidle0', 'load'] }
 			);
+			await page.reload();
 			await new Promise(res => setTimeout(res, 300));
 			await page.bringToFront();
 		});
-
-		after(() => browser.close());
 
 		async function expandDepartment1Node(page) {
 			const options = { delay: 10 };
@@ -49,6 +48,11 @@ import { VisualDiff } from '@brightspace-ui/visual-diff';
 		}
 
 		it('Desktop', async function() {
+			await page.setViewport({
+				width: 600,
+				height: 700,
+				deviceScaleFactor: 2
+			});
 			await expandDepartment1Node(page);
 			const rect = await visualDiff.getRect(page, 'body');
 			await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
@@ -60,6 +64,7 @@ import { VisualDiff } from '@brightspace-ui/visual-diff';
 				height: 700,
 				deviceScaleFactor: 2
 			});
+			await expandDepartment1Node(page);
 			const rect = await visualDiff.getRect(page, 'body');
 			await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
 		});
