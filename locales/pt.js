1155a8911284e9d915c7f178b330f9f91d99a1b0
tie.d2l.dev <TieUser@d2l.com>
2025-05-20 16:37:13 +0000

    Translation Updates

--- locales/pt.js
+++ locales/pt.js
@@ -1,21 +1,21 @@
 /* eslint quotes: 0 */
 
 export default {
-	"orgUnitFilter:nameAllSelected": "Unidade organizacional: Todas",
-	"orgUnitFilter:nameSomeSelected": "Unidade organizacional: Seleções aplicadas",
-	"orgUnitFilter:name": "Unidade organizacional",
+	"orgUnitFilter:nameAllSelected": "Unidade Organizacional: Todas",
+	"orgUnitFilter:nameSomeSelected": "Unidade Organizacional: Seleções Aplicadas",
+	"orgUnitFilter:name": "Unidade Organizacional",
 
-	"treeFilter:nodeName": "{orgUnitName} (ID: {id})",
+	"treeFilter:nodeName": "{orgUnitName} (Id: {id})",
 	"treeFilter:nodeName:root": "raiz",
-	"treeSelector:filterBy": "Filtrar por",
+	"treeSelector:filterBy": "Filtrar Por",
 	"treeSelector:selectAllLabel": "Selecionar tudo",
 	"treeSelector:clearLabel": "Limpar",
 	"treeSelector:searchLabel": "Pesquisar",
-	"treeSelector:loadMoreLabel": "Carregar mais",
+	"treeSelector:loadMoreLabel": "Carregar Mais",
 	"treeSelector:noSearchResults": "Nenhum resultado da pesquisa",
 	"treeSelector:noFiltersAvailable": "Não há filtros disponíveis",
 	"treeSelector:parentLoadMore:ariaLabel": "Carregar mais unidades organizacionais secundárias",
-	"treeSelector:searchLoadMore:ariaLabel": "Carregar mais resultados de pesquisa",
+	"treeSelector:searchLoadMore:ariaLabel": "Carregar mais resultados da pesquisa",
 	"treeSelector:searchPlaceholder": "Pesquisar...",
 	"treeSelector:dropdownAction": "Abrir o filtro {name}",
 	"treeSelector:arrowLabel:closed": "Expandir {name} no nível {level}, secundário de {parentName}",
